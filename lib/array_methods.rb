6f964bbcecede55e6034dbfebe77e529e1a065a0
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-25 00:35:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,11 +11,11 @@ end
 
 def find_max_value(array)
   number = array[0]
-  array.length.times { |index|
+  array.length.times do |index|
     if array[index] > number
       number = array[index]
     end
-  }
+  end
   number
 end
 
