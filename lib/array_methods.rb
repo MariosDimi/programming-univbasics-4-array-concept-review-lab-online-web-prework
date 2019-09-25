459537991d71ff7b06334d36899167996134aee7
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-25 00:36:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,5 +20,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+    number = array[0]
+    array.length.times do |index|
+      if array[index] > number
+        number = array[index]
+      end
+    end
+    number
 end
